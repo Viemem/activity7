884e4db7faaab86e99c974f9b1b0833555478f5c
Guillermo Landa <lcmem08@gmail.com>
2023-03-07 16:15:51 -0600

    generacion modelos-migraciones

---
+++ app/Http/Controllers/MaestroController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\maestro;
+use Illuminate\Http\Request;
+
+class MaestroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\maestro  $maestro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(maestro $maestro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\maestro  $maestro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(maestro $maestro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\maestro  $maestro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, maestro $maestro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\maestro  $maestro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(maestro $maestro)
+    {
+        //
+    }
+}
